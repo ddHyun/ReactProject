9b20685e9516430de8a87a1c342a2df9d1baa1a3
ddHyun <ddHyun@github.com>
2023-11-15 16:17:50 +0900

    초기설정 helmet i18next

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { HelmetProvider } from 'react-helmet-async';
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/langs/en/index.js
@@ -0,0 +1,7 @@
+import common from './common';
+import error from './error';
+import validation from './validation';
+
+const messages = { ...common, ...error, ...validation };
+
+export default messages;
