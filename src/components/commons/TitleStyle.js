e80367f32a751bacf375cf85314feff009c246c3
ddHyun <ddHyun@github.com>
2023-11-16 14:54:19 +0900

    subtitle css

---
+++ src/components/commons/TitleStyle.js
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components';
+import colorNames from '../../styles/colors';
+import sizeNames from '../../styles/sizes';
+
+const { big, extraBig } = sizeNames;
+
+export const MainTitle = styled.h1`
+  font-size: ${extraBig};
+`;
+
+export const SubTitle = styled.h2`
+  font-size: ${big};
+  margin: 0;
+  padding: 0;
+  text-align: ${({ align }) => align || 'left'};
+  color: ${({ color }) => (color ? colorNames.color : '#000')};
+  ${({ borderWidth, color }) =>
+    borderWidth &&
+    css`
+      padding-top: 10px;
+      border-bottom: ${borderWidth}px solid ${color ? color : '#000'};
+    `}
+  margin-bottom: 20px;
+`;
