c46130a887c00eef9d978b8bd441410ee9de4d18
ddHyun <ddHyun@github.com>
2023-11-18 11:19:20 +0900

    Join페이지 구성중&MainTitle css 추가

--- src/components/commons/TitleStyle.js
+++ src/components/commons/TitleStyle.js
@@ -6,6 +6,11 @@ const { big, extraBig } = sizeNames;
 
 export const MainTitle = styled.h1`
   font-size: ${extraBig};
+  padding: 20px 0;
+  margin: 0 0 25px;
+  color: ${({ color }) => (color ? colorNames[color] : '#000')};
+  border-bottom: 2px solid
+    ${({ color }) => (color ? colorNames[color] : '#000')};
 `;
 
 export const SubTitle = styled.h2`
@@ -13,7 +18,7 @@ export const SubTitle = styled.h2`
   margin: 0;
   padding: 0;
   text-align: ${({ align }) => align || 'left'};
-  color: ${({ color }) => (color ? colorNames.color : '#000')};
+  color: ${({ color }) => (color ? colorNames[color] : '#000')};
   ${({ border_width, color }) =>
     border_width &&
     css`
