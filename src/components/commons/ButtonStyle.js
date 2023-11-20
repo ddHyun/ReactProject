1366a8ce205825aafdc134244d7fc00e6e9ee743
ddHyun <ddHyun@github.com>
2023-11-20 13:22:45 +0900

    JoinForm 꾸미는 중

--- src/components/commons/ButtonStyle.js
+++ src/components/commons/ButtonStyle.js
@@ -9,9 +9,22 @@ export const BigButton = styled.button`
   background: ${({ color }) => (color ? colorNames[color] : primary)};
   font-size: ${({ size }) => (size ? sizeNames[size] : big)};
   width: ${({ width }) => width || '100%'};
-  height: ${({ height }) => height || '40px'};
+  height: ${({ height }) => height || 40}px;
   color: ${({ fcolor }) => (fcolor ? colorNames[fcolor] : white)};
-  border: 0;
+  border: 1px solid ${({ bcolor }) => (bcolor ? colorNames[bcolor] : primary)};
   cursor: pointer;
   border-radius: 3px;
+  & + & {
+    margin-left: 5px;
+  }
+`;
+
+export const ButtonGroup = styled.div`
+  width: ${({ width }) => width || 350}px;
+  display: flex;
+  margin: 20px auto;
+  button {
+    flex-grow: 1;
+    width: 0;
+  }
 `;
